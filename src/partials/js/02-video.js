601fa5fc9dd1250b917d42091620ec704ed7f395
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2023-10-11 18:21:25 +0300

    start

--- src/partials/js/02-video.js
+++ src/partials/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const vimeoPlayer = new Player(document.getElementById('vimeo-player'), {
+});
+
+const localStorageKey = 'videoplayer-current-time';
+
+vimeoPlayer.on('timeupdate', throttle(async (data) => {
+  const currentTime = data.seconds;
+  localStorage.setItem(localStorageKey, currentTime.toString());
+}, 1000)); 
+const savedTime = localStorage.getItem(localStorageKey);
+
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
 
--- src/partials/js/03-feedback.js
+++ src/partials/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message');
+
+const localStorageKey = 'feedback-form-state';
+
+function updateLocalStorage() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function populateFormFromLocalStorage() {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  } else {
+    emailInput.value = '';
+    messageTextarea.value = '';
+  }
+}
+
+const throttledUpdateLocalStorage = throttle(updateLocalStorage, 500);
+
+form.addEventListener('input', () => {
+  throttledUpdateLocalStorage();
+});
+populateFormFromLocalStorage();
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  localStorage.removeItem(localStorageKey);
+
+  console.log('Дані форми:', formData);
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
